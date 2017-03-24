474333f9f7992629874af969e1c5cdd8a18b66d7
31 <31@Lenovo-PC>
2017-03-24 18:51:22 -0300

    Primer commit

---
+++ Tp1_programacionIII/src/juego/Ventana_Inicio.java
@@ -0,0 +1,43 @@
+package juego;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Ventana_Inicio {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana_Inicio window = new Ventana_Inicio();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Ventana_Inicio() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
