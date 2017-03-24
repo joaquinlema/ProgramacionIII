5aa9cf772d6f595466c6897a2781e346c03fab7b
31 <31@Lenovo-PC>
2017-03-24 19:13:20 -0300

    Agreado de botones a la ventana

--- Tp1_programacionIII/src/juego/Window_Inicio.java
+++ Tp1_programacionIII/src/juego/Window_Inicio.java
@@ -3,6 +3,7 @@ package juego;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class Window_Inicio {
 
@@ -36,8 +37,47 @@ public class Window_Inicio {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 411);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btn1 = new JButton("1");
+		btn1.setBounds(10, 27, 127, 71);
+		frame.getContentPane().add(btn1);
+		
+		JButton btn2 = new JButton("2");
+		btn2.setBounds(136, 27, 127, 71);
+		frame.getContentPane().add(btn2);
+		
+		JButton btn4 = new JButton("4");
+		btn4.setBounds(10, 97, 127, 71);
+		frame.getContentPane().add(btn4);
+		
+		JButton btn7 = new JButton("7");
+		btn7.setBounds(10, 167, 127, 71);
+		frame.getContentPane().add(btn7);
+		
+		JButton btn3 = new JButton("3");
+		btn3.setBounds(262, 27, 127, 71);
+		frame.getContentPane().add(btn3);
+		
+		JButton btn5 = new JButton("5");
+		btn5.setBounds(136, 97, 127, 71);
+		frame.getContentPane().add(btn5);
+		
+		JButton btn6 = new JButton("6");
+		btn6.setBounds(262, 97, 127, 71);
+		frame.getContentPane().add(btn6);
+		
+		JButton btn8 = new JButton("8");
+		btn8.setBounds(136, 167, 127, 71);
+		frame.getContentPane().add(btn8);
+		
+		JButton btn9 = new JButton("\"\"");
+		btn9.setBounds(262, 167, 127, 71);
+		frame.getContentPane().add(btn9);
+		
+		
 	}
 
 }
