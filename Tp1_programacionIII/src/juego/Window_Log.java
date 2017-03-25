cc32a8aefc1746b233a19698bb613c7933464c07
31 <31@192.168.1.74>
2017-03-25 15:57:35 -0300

    comenzar y salir configurados

--- Tp1_programacionIII/src/juego/Window_Log.java
+++ Tp1_programacionIII/src/juego/Window_Log.java
@@ -7,10 +7,13 @@ import javax.swing.JButton;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Window_Log {
 
 	private JFrame frame;
+	private JTextField txtNombre;
 
 	/**
 	 * Launch the application.
@@ -40,18 +43,37 @@ public class Window_Log {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 230);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.addActionListener(new ActionListener() {
+		JLabel lblNombre = new JLabel("Ingrese Nombre: ");
+		lblNombre.setBounds(40, 54, 108, 18);
+		frame.getContentPane().add(lblNombre);
+		
+		txtNombre = new JTextField();
+		txtNombre.setBounds(153, 53, 208, 20);
+		frame.getContentPane().add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		JButton btnComenzar = new JButton("Comenzar");
+		btnComenzar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Window_Inicio nw = new Window_Inicio();
-				nw.newScreen();
+				Window_Inicio.newScreen();
 				frame.dispose();
 			}
 		});
-		frame.getContentPane().add(btnNewButton, BorderLayout.CENTER);
+		btnComenzar.setBounds(35, 95, 153, 23);
+		frame.getContentPane().add(btnComenzar);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(JFrame.EXIT_ON_CLOSE);
+			}
+		});
+		btnSalir.setBounds(208, 95, 153, 23);
+		frame.getContentPane().add(btnSalir);
 	}
 
 }
